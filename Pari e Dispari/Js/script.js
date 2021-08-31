57be49b31814007113c8c3d01fbb15e572f4accd
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-08-31 17:20:36 +0200

    fix

--- Pari e Dispari/Js/script.js
+++ Pari e Dispari/Js/script.js
@@ -14,8 +14,13 @@ Dichiariamo chi ha vinto. */
     a. chiedere all'utente di scegliere pari o dispari (prompt)
     b. chiedere all'utente di inserisce un numero da 1 a 5 (prompt) */
 
-/* 3. Dichiariamo chi ha vinto
+/* 3. Richiamare le funzioni
     a. confrontare la somma ottenuta se è pari o dispari
+    b. stampare    
+
+/* 4. Dichiariamo chi ha vinto
+    a. confrontare la somma ottenuta se è pari o dispari
+    b. stampare
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
 
@@ -23,17 +28,13 @@ Dichiariamo chi ha vinto. */
 //  a. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
 function RandomNumber(min, max) {
     var randomNumber = Math.floor(Math.random() * (max - min) + 1);
-    console.log(randomNumber);
     return randomNumber;
 }
-RandomNumber(0, 5)
-
 //  b. Sommiamo i due numeri (usando una funzione)
 function sum( num1, num2 ) {    
     var result = num1 + num2; 
     return result; 
 }    
-
 //  c. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 function evenOrOdd( num ) {    
     if (num % 2 == 0) {
@@ -46,9 +47,22 @@ function evenOrOdd( num ) {
 // 2. L'utente sceglie pari o dispari e inserisce un numero da 1 a 5
 //   a. chiedere all'utente di scegliere pari o dispari (prompt)
 var odd_even = prompt("choose even or odd");
+console.log(odd_even);
 // verifica
 //   b. chiedere all'utente di inserisce un numero da 1 a 5 (prompt)
-var number = prompt("choose a number between 1 and 5");
+var number = parseInt(prompt("choose a number between 1 and 5"));
+console.log(number);
 // verifica
 
+// 3. Richiamare le funzioni
+//  a. richiamare il numero computer generato
+var cpuNumber = RandomNumber(0, 5);
+console.log(cpuNumber);
+//  a. sommare il numero utente con il numero computer generato
+var sumUserCpu = sum(number, cpuNumber);
+console.log(sumUserCpu);
 
+
+
+// 4. Dichiariamo chi ha vinto
+//   a. confrontare la somma ottenuta se è pari o dispari
