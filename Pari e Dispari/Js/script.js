c4d77102b449732bf59808fbd6f9dafda09d69b4
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-08-31 16:53:28 +0200

    random function

--- Pari e Dispari/Js/script.js
+++ Pari e Dispari/Js/script.js
@@ -4,3 +4,26 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
+
+/* 1. Dichiarare le funzioni 
+    a. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
+    b. Sommiamo i due numeri (usando una funzione)
+    c. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) */
+
+/* 2. L'utente sceglie pari o dispari e inserisce un numero da 1 a 5
+    a. chiedere all'utente di scegliere pari o dispari
+    b. chiedere all'utente (prompt) */
+
+/* 3. Dichiariamo chi ha vinto
+    a. confrontare la somma ottenuta se è pari o dispari
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+// 1. Dichiarare le funzioni 
+//  a. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
+function RandomNumber(min, max) {
+    var randomNumber = Math.floor(Math.random() * (max - min) + 1);
+    console.log(randomNumber);
+    return randomNumber;
+}
+RandomNumber(0, 5)
